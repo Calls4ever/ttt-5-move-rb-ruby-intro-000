af4dff60ddb0220f47c04e08c03459effe5ed1b9
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-08 21:15:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,3 @@
-
-def move(board, user_input, value ="X")
-  board[input] = value
-  display_board
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
